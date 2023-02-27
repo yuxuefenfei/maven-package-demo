06505eea7953002e84c964415505700a13e62ec6
yuxuefenfei0@gmail.com <yuxuefenfei0@gmail.com>
2023-02-27 14:57:09 +0800

    init

---
+++ src/main/java/cn/wangwenzhu/MainApp.java
@@ -0,0 +1,13 @@
+package cn.wangwenzhu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainApp {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainApp.class);
+
+    public static void main(String[] args) {
+        logger.info("打包程序启动成功");
+    }
+}
